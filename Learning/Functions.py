6d2bd1802f9eb1382956d86238cece8bc313d1ea
Divine Jude <judedivine68@gmail.com>
2022-10-21 20:18:13 +0100

    Update Functions.py

--- Learning/Functions.py
+++ Learning/Functions.py
@@ -101,25 +101,25 @@ musician = build_person('jimi', 'hendrix', age=27)
 print(musician)
 
 
-# using a function with a while loop
-# def get_formatted_name(first_name, last_name):
-#     """Return a fullname, neatly formatted."""
-#     full_name = f"{first_name} {last_name}"
-#     return full_name.title()
-#
-#
-# # This is an infinite loop!
-# while True:
-#     print("\nPlease tell me your name:")
-#     print("(Enter 'quit' at any time to quit)")
-#     f_name = input("First name: ")
-#     if f_name == 'quit':
-#         break
-#     l_name = input("Last name: ")
-#     if l_name == 'quit':
-#         break
-#     formatted_name = get_formatted_name(f_name, l_name)
-#     print(f"Hello, {formatted_name.title()}!")
+using a function with a while loop
+def get_formatted_name(first_name, last_name):
+    """Return a fullname, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+
+# This is an infinite loop!
+while True:
+    print("\nPlease tell me your name:")
+    print("(Enter 'quit' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == 'quit':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'quit':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"Hello, {formatted_name.title()}!")
 
 
 # Passing a List
